cf0552d6a22162359bc87882d6effefa601fc81a
Cody Sherrod <w.cody.sherrod@gmail.com>
2018-06-19 16:40:10 -0500

    making some changes to allow connection to mLab

--- server.js
+++ server.js
@@ -16,7 +16,10 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes)
 
 // Connect to the Mongo DB
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nytreact')
+let MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/nytreact'
+
+mongoose.Promise = Promise
+mongoose.connect(MONGODB_URI)
 
 // Start the API server
 app.listen(PORT, function () {
